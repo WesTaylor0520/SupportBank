9191c93b5192684007c21c47bffeb2429360ee76
Wes Taylor <wes.taylor@capgemini.com>
2022-09-29 11:14:51 +0100

    Date and Amount validation added

--- main.py
+++ main.py
@@ -1,54 +1,75 @@
 import csv
 from decimal import *
 import logging
+from dateutil.parser import *
 
 logging.basicConfig(filename="SupportBank.log", filemode="w", level=logging.DEBUG)
 logging.info("Code started")
 
 
+def validateDate(date, row, file, fuzzy=False):
+    try:
+        parse(date, fuzzy=fuzzy)
+        return True
+    except ValueError:
+        logging.info(f"Incorrect formatting of {date} in Date column of row {row} in input {file}")
+        logging.info(f"Row {row} in input {file} has been skipped")
+        return False
+
+
+def validateAmount(amount, row, file):
+    try:
+        Decimal(amount)
+        return True
+    except InvalidOperation:
+        logging.info(f"Incorrect formatting of {amount} in Amount column of row {row} in input {file}")
+        logging.info(f"Row {row} in input {file} has been skipped")
+        return False
+
+
 def addToBalanceDict(file):
     BalanceDict = {}
+    rowCounter = 0
     with open(file, mode="r") as csv_file:
         csv_reader = csv.DictReader(csv_file)
         for row in csv_reader:
-            if row["From"] not in BalanceDict.keys():
-                BalanceDict[row["From"]] = 0
-            if row["To"] not in BalanceDict.keys():
-                BalanceDict[row["To"]] = 0
+            date = row["Date"]
+            debt = row["Amount"]
+            if validateAmount(debt, rowCounter, file) and validateDate(date, rowCounter, file):
+                debtDecimal = Decimal(debt)
 
-            debt = Decimal(row["Amount"])
+                if row["From"] not in BalanceDict.keys():
+                    BalanceDict[row["From"]] = 0
+                if row["To"] not in BalanceDict.keys():
+                    BalanceDict[row["To"]] = 0
 
-            fromAccountChange = Decimal(BalanceDict[row["From"]])
-            fromAccountChange -= debt
-            BalanceDict[row["From"]] = fromAccountChange
+                fromAccountChange = Decimal(BalanceDict[row["From"]])
+                fromAccountChange -= debtDecimal
+                BalanceDict[row["From"]] = fromAccountChange
 
-            ToAccountChange = Decimal(BalanceDict[row["To"]])
-            ToAccountChange += debt
-            BalanceDict[row["To"]] = ToAccountChange
+                ToAccountChange = Decimal(BalanceDict[row["To"]])
+                ToAccountChange += debtDecimal
+                BalanceDict[row["To"]] = ToAccountChange
+            rowCounter += 1
     return BalanceDict
 
 
 def listAll():
-    try:
-        firstCSV = addToBalanceDict("Transactions2014.txt")
-        logging.info("firstCSV Created")
-    except InvalidOperation:
-        logging.info("firstCSV failed to be created")
-
-    try:
-        secondCSV = addToBalanceDict("DodgyTransactions2015.txt")
-    except InvalidOperation:
-        logging.info("secondCSV failed to be created")
+    firstCSV = addToBalanceDict("Transactions2014.csv")
+    print("\nFirst CSV \n")
 
     for k, v in firstCSV.items():
         print(k, v)
+
+    secondCSV = addToBalanceDict("DodgyTransactions2015.csv")
+    print("\nSecond CSV \n")
 
     for k, v in secondCSV.items():
         print(k, v)
 
 
 def listAccount(inputName):
-    with open("Transactions2014.txt", mode="r") as csv_file:
+    with open("Transactions2014.csv", mode="r") as csv_file:
         csv_reader = csv.DictReader(csv_file)
 
         for row in csv_reader:
